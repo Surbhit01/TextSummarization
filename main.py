df6cacf2540dac63af8fbc6a88ab97ce30d2806e
Surbhit01 <surbhit3812@gmail.com>
2023-06-09 01:35:06 +0530

    Data Validation stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from textSummarization.logging import logger
 from textSummarization.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarization.pipeline.state_02_data_validation import DataValidationTrainingPipeline
 
 
 STATE_NAME = "Data Ingestion"
@@ -7,6 +8,17 @@ try:
     logger.info(f">>>>>state {STATE_NAME} started<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>state {STATE_NAME} completed<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STATE_NAME = "Data Validation"
+try:
+    logger.info(f">>>>>state {STATE_NAME} started<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f">>>>>state {STATE_NAME} completed<<<<<")
 except Exception as e:
     logger.exception(e)
